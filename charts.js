0c4d74c8d387c145c7217fa7348ed2e4ebcae873
Brittanie <brittaniejones@ymail.com>
2022-04-30 12:53:50 -0700

    Fixed D1

--- charts.js
+++ charts.js
@@ -57,6 +57,7 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
+    
     // 3. Create a variable that holds the samples array. 
     var samples = data.samples;
 
